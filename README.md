f8df70df0c544aa4a8c18cb3ae79b4e21c027aa0
lei.cai <lei.cai@aiwinn.com>
2020-11-09 08:52:13 +0800

    1.更新README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# AiVoice 项目
+# AiVoice
+
+1. 初始化项目
+
+2. 组件化架构搭建
+
+3. base 类的编写
+
+   ~~~
+   1.BaseActivity,BaseFragment 封装
+   2.LogUtils,SpUtils 封装
+   3.BaseAdapter ViewHolder 封装
+   ~~~
+
+4.
+
--- lib_base/src/main/java/com/example/lib_base/base/BaseActivity.kt
+++ lib_base/src/main/java/com/example/lib_base/base/BaseActivity.kt
@@ -1,6 +1,14 @@
 package com.example.lib_base.base
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId())
+    }
+
+    abstract fun getLayoutId(): Int
 }
